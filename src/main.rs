69406d28c46be212544537ab485a5af254643492
Victor Panayotov <victorpanayotov@gmail.com>
2026-01-04 18:04:50 +0000

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-// #![feature(iter_map_windows)]
-mod print;
-
 use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
 
-use llvm_compiler::parse::parser::Parser;
 use llvm_compiler::{lexer::AsciiLexer, token::Token};
 
 pub const READER_CAPACITY: usize = 100_000_000;
@@ -20,11 +16,11 @@ fn main() -> miette::Result<()> {
     let mut buf = String::new();
 
     // Read in as much as we can at once
-    let bytes_read = reader.read_to_string(&mut buf).unwrap(); // TODO: There is some edge
-                                                               // case behaviour here when
-                                                               // a file is too long to
-                                                               // store in memory. This is
-                                                               // currently unhandled.
+    let _ = reader.read_to_string(&mut buf).unwrap(); // TODO: There is some edge
+                                                      // case behaviour here when
+                                                      // a file is too long to
+                                                      // store in memory. This is
+                                                      // currently unhandled.
 
     let tokens: Vec<Token<'_>> = lexer.tokenize(&buf);
 
@@ -35,15 +31,6 @@ fn main() -> miette::Result<()> {
     println!("{}", tokens.len());
 
     println!();
-
-    let mut parser = Parser::new(0, &tokens);
-
-    let source = buf.clone();
-    // let parsed = parse(&mut parser).map_err(|report| report.with_source_code(source));
-
-    // println!("{:?}", parsed);
-
-    // print::print_parsed_tree(&parsed.unwrap());
 
     Ok(())
 }
